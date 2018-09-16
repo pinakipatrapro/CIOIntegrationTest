08757692c4ccef8b1c52735925b201c2ab9144b2
Pinaki Patra <pinaki.patra.pro@gmail.com>
2018-09-16 11:31:26 +0530

    Test Commit to run in EC2

--- src/test/java/util/loadWebdriver.java
+++ src/test/java/util/loadWebdriver.java
@@ -7,7 +7,7 @@ public class loadWebdriver {
 	static WebDriver driver;
 	public WebDriver getDriver() {
 		if (driver == null){
-			System.setProperty("webdriver.gecko.driver","C:\\Users\\pinaki.patra\\Documents\\Project Documents\\Tools\\geckodriver-v0.21.0-win64\\geckodriver.exe");
+//			System.setProperty("webdriver.gecko.driver","C:\\Users\\pinaki.patra\\Documents\\Project Documents\\Tools\\geckodriver-v0.21.0-win64\\geckodriver.exe");
 			driver = new FirefoxDriver();
 			return driver;
 		}else{
