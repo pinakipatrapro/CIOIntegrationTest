a678af3197d0d84451de1c80d9ecbbdafa5397c7
Pinaki Patra <pinaki.patra.pro@gmail.com>
2018-10-11 17:26:56 +0530

    Added Proxy  Config
    Added Proxy  Config

--- src/test/java/util/loadWebdriver.java
+++ src/test/java/util/loadWebdriver.java
@@ -2,11 +2,19 @@ package util;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
 
 public class loadWebdriver {
 	static WebDriver driver;
 	public WebDriver getDriver() {
 		if (driver == null){
+			
+			FirefoxProfile profile = new FirefoxProfile();
+			profile.setPreference("network.proxy.http", "localhost");
+			profile.setPreference("network.proxy.http", "localhost");
+			
+			
+			
 			System.setProperty("webdriver.gecko.driver","C:\\Driver\\geckodriver.exe"); 
 			driver = new FirefoxDriver();
 			return driver;
